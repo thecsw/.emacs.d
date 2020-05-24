6705ed1fa4b541d9f1f5d7949e70564720a969ef
Sandy House <ctu@ku.edu>
2020-05-24 03:01:39 -0500

    Removed my colors, lush is good enough
    Signed-off-by: Sandy House <ctu@ku.edu>

--- configuration.el
+++ configuration.el
@@ -100,23 +100,6 @@ There are two things you can do about this warning:
 
 ;; Press C-x g to open magit
 (global-set-key (kbd "C-x g") 'magit-status)
-;; Define some colors for magit, I love pink
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
- '(diff-function ((t (:inherit nil))))
- '(line-number ((t (:inherit nil))))
- '(linum ((t (:background "unspecified-bg" :foreground "#565761" :slant italic))))
- '(magit-diff-added ((t (:foreground "#22aa22"))))
- '(magit-diff-added-highlight ((t (:background "color-233" :foreground "#22aa22"))))
- '(magit-diff-context-highlight ((t (:background "color-233" :foreground "brightyellow"))))
- '(magit-diff-file-heading-highlight ((t (:foreground "magenta"))))
- '(magit-diff-removed ((t (:foreground "#aa2222"))))
- '(magit-diff-removed-highlight ((t (:background "color-233" :foreground "#aa2222"))))
- '(magit-section-highlight ((t (:background "color-232" :foreground "brightmagenta" :weight bold)))))
 
 (require 'powerline)
 ;; Use the vim powerline theme
@@ -157,3 +140,5 @@ There are two things you can do about this warning:
 (setq org-superstar-leading-bullet ?\s)
 ;; Add the new fancy extra org mode blocks
 (add-hook 'org-mode-hook #'org-special-block-extras-mode)
+
+(setq olivetti-body-width 80)
