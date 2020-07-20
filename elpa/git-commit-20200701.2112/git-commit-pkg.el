e3908c1d7d31a2967c01117f38c60bbf73bfc090
Sandy FedoraX <ctu@ku.edu>
2020-07-19 19:10:28 -0500

    Fixed the fucking error with flymake not making proper eldoc documentation calls that. Don't use flymake 1.0.9

---
+++ elpa/git-commit-20200701.2112/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/thecsw/.emacs.d/elpa/git-commit-20200701.2112/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200701.2112" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "dc8a24b4cdb4c6371d3f52a5105f9887bbcc371b" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
---
+++ elpa/language-id-20200714.923/language-id-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/thecsw/.emacs.d/elpa/language-id-20200714.923/language-id.el  -*- no-byte-compile: t -*-
+(define-package "language-id" "20200714.923" "Library to work with programming language identifiers" '((emacs "24") (cl-lib "0.5")) :commit "1b37f24f2bfd70cd0856c5293cd30f262c7ac21a" :keywords '("languages" "util") :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :url "https://github.com/lassik/emacs-language-id")
