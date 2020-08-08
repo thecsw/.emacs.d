5acda9a7820e90d4c683ea601f472fed26a693e1
Sandy FedoraX <ctu@ku.edu>
2020-08-08 18:58:51 -0500

    Deleted obsolete packages
    Signed-off-by: Sandy FedoraX <ctu@ku.edu>

--- elpa/goto-line-preview-20190308.736/goto-line-preview-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/thecsw/.emacs.d/elpa/goto-line-preview-20190308.736/goto-line-preview.el  -*- no-byte-compile: t -*-
-(define-package "goto-line-preview" "20190308.736" "Preview line when executing `goto-line` command." '((emacs "25")) :commit "1f0afb261a4e4a1b0a2fae3959b0ce5d30bce2a1" :authors '(("Shen, Jen-Chieh" . "jcs090218@gmail.com")) :maintainer '("Shen, Jen-Chieh" . "jcs090218@gmail.com") :url "https://github.com/jcs090218/goto-line-preview")
--- elpa/w3m-20200325.2226/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20200325.2226" "an Emacs interface to w3m" 'nil :commit "e522a44b151d69d4a176e8809225598ab540fc4d" :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
