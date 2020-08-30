b84fdaaabf5dd8ee8cd7c45127c02df427bf544e
Sandy FedoraX <ctu@ku.edu>
2020-08-30 15:34:05 -0500

    bump
    Signed-off-by: Sandy FedoraX <ctu@ku.edu>

--- elpa/which-key-20200817.2358/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/thecsw/.emacs.d/elpa/which-key-20200817.2358/which-key.el  -*- no-byte-compile: t -*-
-(define-package "which-key" "20200817.2358" "Display available keybindings in popup" '((emacs "24.4")) :commit "e48e190a75a0c176e1deac218b891e77792d6921" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
