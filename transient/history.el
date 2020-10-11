ab0e9b13d0f68bcf7ef896c031f49cdf7936085b
Sandy FedoraX <ctu@ku.edu>
2020-10-11 14:23:48 -0500

    bump
    Signed-off-by: Sandy FedoraX <ctu@ku.edu>

--- transient/history.el
+++ transient/history.el
@@ -4,7 +4,12 @@
  (docker-image-rm nil
 		  ("-f"))
  (docker-network-rm nil)
+ (magit-am
+  ("--3way" "--signoff")
+  ("--3way"))
  (magit-bisect nil)
+ (magit-cherry-pick
+  ("--ff"))
  (magit-commit
   ("--signoff")
   ("--allow-empty" "--signoff")
@@ -16,6 +21,7 @@
  (magit-log
   ("-n256" "--graph" "--decorate"))
  (magit-merge nil)
+ (magit-patch-apply nil)
  (magit-push nil)
  (magit-stash nil)
  (magit-tag nil
