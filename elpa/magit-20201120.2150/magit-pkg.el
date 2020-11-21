9644dbc0a114ab2848190875e3830845a5744988
Sandy Urazayev <ctu@ku.edu>
2020-11-21 03:11:02 -0600

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/magit-20201120.2150/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20201120.2150" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "359d0f2ffbb0719e4b79ab4c2e0acddfb2b9aa88" :keywords
+  ("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/yasnippet-snippets-20201118.2148/yasnippet-snippets-pkg.el
@@ -0,0 +1,12 @@
+(define-package "yasnippet-snippets" "20201118.2148" "Collection of yasnippet snippets"
+  '((yasnippet "0.8.0"))
+  :commit "9fc9c2144aa3db4d85fdd35f7df28e58aa1801b3" :keywords
+  ("snippets")
+  :authors
+  (("Andrea Crotti" . "andrea.crotti.0@gmail.com"))
+  :maintainer
+  ("Andrea Crotti" . "andrea.crotti.0@gmail.com")
+  :url "https://github.com/AndreaCrotti/yasnippet-snippets")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
