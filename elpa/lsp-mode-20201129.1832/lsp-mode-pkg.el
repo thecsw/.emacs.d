d72c0dd75bf862a36fc707c2ce60b8b078ec78b8
Sandy Urazayev <ctu@ku.edu>
2020-11-29 14:23:34 -0600

    Updated melpa source and bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/lsp-mode-20201129.1832/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20201129.1832" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "5005ebea57cd1e05f924d4e5653c1abc9fe212dd" :keywords
+  ("languages")
+  :authors
+  (("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  ("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
