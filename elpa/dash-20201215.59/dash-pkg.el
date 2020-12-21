1f4da6488f42ad91080ff5b1d81c88bce3e75586
Sandy Urazayev <ctu@ku.edu>
2020-12-22 02:56:27 +0400

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- elpa/dash-20201215.59/dash-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "dash" "20201215.59" "A modern list library for Emacs" 'nil :commit "7629dae97d6648ef560569067801a3b91028c1c0" :keywords
-  ("extensions" "lisp")
-  :authors
-  (("Magnar Sveen" . "magnars@gmail.com"))
-  :maintainer
-  ("Magnar Sveen" . "magnars@gmail.com"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/prettier-20201221.1105/prettier-pkg.el
@@ -0,0 +1,14 @@
+(define-package "prettier" "20201221.1105" "Code formatting with Prettier"
+  '((emacs "26.1")
+    (iter2 "0.9")
+    (nvm "0.2"))
+  :commit "df9119858ac0edfff5bd6aedb8d708da73832c14" :keywords
+  ("convenience" "languages" "files")
+  :authors
+  (("Julian Scheid" . "julians37@gmail.com"))
+  :maintainer
+  ("Julian Scheid" . "julians37@gmail.com")
+  :url "https://github.com/jscheid/prettier.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
