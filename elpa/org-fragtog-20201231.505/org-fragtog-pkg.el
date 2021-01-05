2c70eff550c9e0b74e009cd91109b5f6eebf5052
Sandy Urazayev <ctu@ku.edu>
2021-01-05 18:19:16 +0400

    improvements, added s5 presentations, fold org files when opening...
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/org-fragtog-20201231.505/org-fragtog-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-fragtog.el  -*- no-byte-compile: t -*-
+(define-package "org-fragtog" "20201231.505" "Auto-toggle Org LaTeX fragments" '((emacs "24.3") (org "9.3.2")) :commit "0151cabc7aa9f244f82e682b87713b344d780c23" :authors '(("Benjamin Levy" . "blevy@protonmail.com")) :maintainer '("Benjamin Levy" . "blevy@protonmail.com") :url "https://github.com/io12/org-fragtog")
