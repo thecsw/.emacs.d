90cd8d90964ade36d3bf522218a7f23e14b2f3a1
Sandy Urazayev <ctu@ku.edu>
2021-01-13 00:52:49 +0400

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/emms-20210111.251/emms-pkg.el
@@ -0,0 +1,13 @@
+(define-package "emms" "20210111.251" "The Emacs Multimedia System"
+  '((cl-lib "0.5")
+    (seq "0"))
+  :commit "5c3226bec64bc5ad6a496b1619144087ba400481" :authors
+  '(("Jorgen Schäfer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Yoni Rabkin" . "yrk@gnu.org")
+  :keywords
+  '("emms" "mp3" "ogg" "flac" "music" "mpeg" "video" "multimedia")
+  :url "https://www.gnu.org/software/emms/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/posframe-20210110.54/posframe-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from posframe.el  -*- no-byte-compile: t -*-
-(define-package "posframe" "20210110.54" "Pop a posframe (just a frame) at point" '((emacs "26")) :commit "e6c6bdb10a8d99863951a3a2db2f030577cb9162" :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :keywords '("convenience" "tooltip") :url "https://github.com/tumashu/posframe")
