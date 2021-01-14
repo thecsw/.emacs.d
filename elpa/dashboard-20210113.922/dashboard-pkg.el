987f48073c3f7d1ca1820a3fa4741c46443b954b
Sandy Urazayev <ctu@ku.edu>
2021-01-15 02:49:36 +0400

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/dashboard-20210113.922/dashboard-pkg.el
@@ -0,0 +1,13 @@
+(define-package "dashboard" "20210113.922" "A startup screen extracted from Spacemacs"
+  '((emacs "25.3")
+    (page-break-lines "0.11"))
+  :commit "fb5e47576a1c91374b008221277f0a45b2491085" :authors
+  '(("Rakan Al-Hneiti"))
+  :maintainer
+  '("Rakan Al-Hneiti")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20210114.1955/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20210114.1955" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "0062a7cd4ab674390287e6b20d93ac42ed1f5512" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
