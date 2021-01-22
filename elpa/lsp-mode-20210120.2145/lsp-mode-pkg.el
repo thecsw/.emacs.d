8df1ae8fa9d71758df719a354a2f61a69836a1b5
Sandy Urazayev <ctu@ku.edu>
2021-01-21 22:06:34 -0600

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/lsp-mode-20210120.2145/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20210120.2145" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "4bc9e9d811dd7ee8541882f1a0cd8bb81e3e3bda" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-20210121.512/magit-pkg.el
@@ -0,0 +1,16 @@
+(define-package "magit" "20210121.512" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "0390cebda94e0b2937f7c56a9b0c676dabd12d44" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
