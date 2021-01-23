9df75d1c2f5c01846a378c750a7918f18509b526
Sandy Urazayev <ctu@ku.edu>
2021-01-23 16:57:44 -0600

    removed emms
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- elpa/emms-state-20160504.805/emms-state-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/thecsw/.emacs.d/elpa/emms-state-20160504.805/emms-state.el  -*- no-byte-compile: t -*-
-(define-package "emms-state" "20160504.805" "Display track description and playing time in the mode line" '((emms "0")) :commit "77930300222333b71eafd495cc1fee3a3585eb23" :keywords '("emms") :authors '(("Alex Kost" . "alezost@gmail.com")) :maintainer '("Alex Kost" . "alezost@gmail.com") :url "https://github.com/alezost/emms-state.el")
