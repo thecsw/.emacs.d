7a403f52f6bb4d3caa8e2dff772dabb27ea3ffbf
Sandy Urazayev <ctu@ku.edu>
2021-01-23 17:06:09 -0600

    Added hackernews
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/hackernews-20210117.1834/hackernews-autoloads.el
@@ -0,0 +1,29 @@
+;;; hackernews-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "hackernews" "hackernews.el" (0 0 0 0))
+;;; Generated autoloads from hackernews.el
+
+(autoload 'hackernews "hackernews" "\
+Read top N Hacker News stories.
+The Hacker News feed is determined by `hackernews-default-feed'
+and N defaults to `hackernews-items-per-page'.
+
+\(fn &optional N)" t nil)
+
+(register-definition-prefixes "hackernews" '("hackernews-"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hackernews-autoloads.el ends here
---
+++ elpa/hackernews-20210117.1834/hackernews-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from hackernews.el  -*- no-byte-compile: t -*-
+(define-package "hackernews" "20210117.1834" "Hacker News Client for Emacs" 'nil :commit "c9c2bb0f13f5bd64c74dbdf945d9613192c0e454" :authors '(("Lincoln de Sousa" . "lincoln@comum.org")) :maintainer '("Basil L. Contovounesios" . "contovob@tcd.ie") :keywords '("comm" "hypermedia" "news") :url "https://github.com/clarete/hackernews.el")
