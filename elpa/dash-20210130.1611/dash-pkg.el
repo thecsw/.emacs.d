58f568451b6b428987aead2015269e2f9dcd419d
Sandy Urazayev <ctu@ku.edu>
2021-01-30 16:32:37 -0600

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/dash-20210130.1611/dash-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dash" "20210130.1611" "A modern list library for Emacs"
+  '((emacs "24"))
+  :commit "baf91479fd83f9ddcd030e3f730a21c55f1e6523" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/magnars/dash.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
