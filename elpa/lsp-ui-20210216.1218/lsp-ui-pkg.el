3257d5a3c0abca549a76ff4dcf193eeaca8d0943
Sandy Urazayev <ctu@ku.edu>
2021-02-17 20:20:39 -0600

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/lsp-ui-20210216.1218/lsp-ui-pkg.el
@@ -0,0 +1,15 @@
+(define-package "lsp-ui" "20210216.1218" "UI modules for lsp-mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3"))
+  :commit "78e0a41c9b6b0a166c6c86d2541f310883d15b68" :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :keywords
+  '("languages" "tools")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
