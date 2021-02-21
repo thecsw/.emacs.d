d615186c46975661c8ff4d02dbb5a0461a6ced99
Sandy Urazayev <ctu@ku.edu>
2021-02-21 16:27:19 -0600

    Added neotree with all-the-icons
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/dashboard-20210218.1550/dashboard-pkg.el
@@ -0,0 +1,13 @@
+(define-package "dashboard" "20210218.1550" "A startup screen extracted from Spacemacs"
+  '((emacs "25.3")
+    (page-break-lines "0.11"))
+  :commit "01270b935b969785530303134cdfd2c1d15606cc" :authors
+  '(("Rakan Al-Hneiti"))
+  :maintainer
+  '("Rakan Al-Hneiti")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/deadgrep-20210113.829/deadgrep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from deadgrep.el  -*- no-byte-compile: t -*-
-(define-package "deadgrep" "20210113.829" "fast, friendly searching with ripgrep" '((emacs "25.1") (dash "2.12.0") (s "1.11.0") (spinner "1.7.3")) :commit "bb374c625939ac2dd9e2408c2cc180b1c9f4aa20" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :keywords '("tools") :url "https://github.com/Wilfred/deadgrep")
