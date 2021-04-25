d34bc30162175115cf0a482312d3cc8a6b34acd0
Sandy Urazayev <ctu@ku.edu>
2021-04-25 03:10:05 -0500

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/dashboard-20210423.803/dashboard-pkg.el
@@ -0,0 +1,13 @@
+(define-package "dashboard" "20210423.803" "A startup screen extracted from Spacemacs"
+  '((emacs "25.3")
+    (page-break-lines "0.11"))
+  :commit "caad6f4b75d6645115dae8e66a96366f678527b8" :authors
+  '(("Rakan Al-Hneiti"))
+  :maintainer
+  '("Rakan Al-Hneiti")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/w3m-20210409.626/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20210409.626" "an Emacs interface to w3m" 'nil :commit "8bab3dc89d36b55ba26ae5941f294c57805d24b2" :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
