2324d729b14cff0d7470beba4c73621a009ec7ce
Sandy Urazayev <ctu@ku.edu>
2021-07-04 23:07:04 -0500

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/helm-core-20210703.1610/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20210703.1610" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "32e6d6b765644f73793f1c6cf60d41149d31e3aa")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-section-20210702.822/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210702.822" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210330"))
+  :commit "b6751568c32b87f8b98c304531ad7f20799c6d86" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
