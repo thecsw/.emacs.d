18d2f95ab28c87ce2c15a004b93f302d35ac9e21
Sandy Urazayev <ctu@ku.edu>
2021-07-24 17:29:01 -0500

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/telephone-line-20210724.1411/telephone-line-pkg.el
@@ -0,0 +1,15 @@
+(define-package "telephone-line" "20210724.1411" "Rewrite of Powerline"
+  '((emacs "24.4")
+    (cl-lib "0.5")
+    (cl-generic "0.2")
+    (seq "1.8"))
+  :commit "ff5fcb2181cf1d52bfc5fb8d76ac37f9cad22ce2" :authors
+  '(("Daniel Bordak" . "dbordak@fastmail.fm"))
+  :maintainer
+  '("Daniel Bordak" . "dbordak@fastmail.fm")
+  :keywords
+  '("mode-line")
+  :url "https://github.com/dbordak/telephone-line")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
