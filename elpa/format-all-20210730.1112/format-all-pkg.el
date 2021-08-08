ad4cac0aaa6b6640f4ac6a83ba15a60d430a912e
Sandy Urazayev <ctu@ku.edu>
2021-08-09 01:59:50 +0600

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- elpa/format-all-20210730.1112/format-all-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from format-all.el  -*- no-byte-compile: t -*-
-(define-package "format-all" "20210730.1112" "Auto-format C, C++, JS, Python, Ruby and 50 other languages" '((emacs "24.3") (inheritenv "0.1") (language-id "0.15")) :commit "b18ed67a81b4d81d77f3bd7cf6e004e4e412b630" :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :keywords '("languages" "util") :url "https://github.com/lassik/emacs-format-all-the-code")
---
+++ elpa/w3m-20210805.2339/w3m-pkg.el
@@ -0,0 +1,5 @@
+(define-package "w3m" "20210805.2339" "an Emacs interface to w3m" 'nil :commit "f77da8bdbc75af33f941ffb8b5de8d423e04a0f4" :keywords
+  '("w3m" "www" "hypermedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
