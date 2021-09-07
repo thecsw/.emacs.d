b4a4e08391bdeea04dcf5805a1b28249d47fc9bc
Sandy Urazayev <ctu@ku.edu>
2021-09-07 13:38:37 -0500

    bump
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

---
+++ elpa/w3m-20210906.347/w3m-pkg.el
@@ -0,0 +1,5 @@
+(define-package "w3m" "20210906.347" "an Emacs interface to w3m" 'nil :commit "ef34cf7f806a726a53fcffa183280a325defbc54" :keywords
+  '("w3m" "www" "hypermedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
