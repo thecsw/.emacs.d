d4a5baa2beee7a34304472607626d7fa508ab5e3
Sandy <sandy@sandyuraz.com>
2022-07-12 15:29:57 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- elpa/org-fragtog-20220705.127/org-fragtog-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from org-fragtog.el  -*- no-byte-compile: t -*-
-(define-package "org-fragtog" "20220705.127" "Auto-toggle Org LaTeX fragments" '((emacs "27.1")) :commit "9349b465f1b5e61d0fb4a942ff6ce3f69a426097" :authors '(("Benjamin Levy" . "blevy@protonmail.com")) :maintainer '("Benjamin Levy" . "blevy@protonmail.com") :url "https://github.com/io12/org-fragtog")
---
+++ elpa/org-fragtog-20220708.129/org-fragtog-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-fragtog.el  -*- no-byte-compile: t -*-
+(define-package "org-fragtog" "20220708.129" "Auto-toggle Org LaTeX fragments" '((emacs "27.1")) :commit "ede4ac61e8cb580f63f882bdee2b0e573cd3dd60" :authors '(("Benjamin Levy" . "blevy@protonmail.com")) :maintainer '("Benjamin Levy" . "blevy@protonmail.com") :url "https://github.com/io12/org-fragtog")
