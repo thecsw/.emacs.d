6ca821ad83af596c3e903b3334301adbd1d51fc5
Sandy <sandy@sandyuraz.com>
2022-08-04 15:51:35 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/typescript-mode-20220730.1035/typescript-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "typescript-mode" "20220730.1035" "Major mode for editing typescript"
+  '((emacs "24.3"))
+  :commit "acd8d7995204c1faf14383b8236e57f4da380ecf" :keywords
+  '("typescript" "languages")
+  :url "http://github.com/ananthakumaran/typescript.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/w3m-20220729.42/w3m-pkg.el
@@ -0,0 +1,5 @@
+(define-package "w3m" "20220729.42" "an Emacs interface to w3m" 'nil :commit "db02ce0fde628a8eb9a1a0c95939c41981fdd931" :keywords
+  '("w3m" "www" "hypermedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
