486c4906e480a9b383ee1b0bee094b25ec3fb969
Sandy <sandy@sandyuraz.com>
2023-01-31 21:47:06 -0600

    Added yabaki-theme and eyebrowse
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/yabaki-theme-20230117.2312/yabaki-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; yabaki-theme-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "yabaki-theme" "yabaki-theme.el" (0 0 0 0))
+;;; Generated autoloads from yabaki-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory (or load-file-name buffer-file-name)))))
+
+(register-definition-prefixes "yabaki-theme" '("yabaki"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; yabaki-theme-autoloads.el ends here
