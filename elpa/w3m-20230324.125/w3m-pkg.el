27a772cb32814acc80c21ea9022dac2ab96d06d4
Sandy <sandy@sandyuraz.com>
2023-08-06 22:16:17 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- elpa/w3m-20230324.125/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20230324.125" "an Emacs interface to w3m" 'nil :commit "f8819bca0c97970662cd4214f8f3104ab4a376c1" :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
