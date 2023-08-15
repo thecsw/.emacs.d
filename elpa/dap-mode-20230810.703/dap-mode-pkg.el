eeaf94ebaa19b3d8ada9e46de0c550557dfc31de
Sandy <sandy@sandyuraz.com>
2023-08-14 23:05:03 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/dap-mode-20230810.703/dap-mode-pkg.el
@@ -0,0 +1,23 @@
+(define-package "dap-mode" "20230810.703" "Debug Adapter Protocol mode"
+  '((emacs "27.1")
+    (dash "2.18.0")
+    (lsp-mode "6.0")
+    (bui "1.1.0")
+    (f "0.20.0")
+    (s "1.12.0")
+    (lsp-treemacs "0.1")
+    (posframe "0.7.0")
+    (ht "2.3")
+    (lsp-docker "1.0.0"))
+  :commit "685276371566d90b585c4acc19139d7fb2980f63" :authors
+  '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
+  :maintainers
+  '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
+  :maintainer
+  '("Ivan Yonchovski" . "yyoncho@gmail.com")
+  :keywords
+  '("languages" "debug")
+  :url "https://github.com/emacs-lsp/dap-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
