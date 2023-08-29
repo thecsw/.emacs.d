7025486fd59617d3583532aa8397188ab620648f
Sandy <sandy@sandyuraz.com>
2023-08-29 00:00:32 -0500

    bump

---
+++ elpa/docker-20230823.937/docker-pkg.el
@@ -0,0 +1,19 @@
+(define-package "docker" "20230823.937" "Interface to Docker"
+  '((aio "1.0")
+    (dash "2.19.1")
+    (emacs "26.1")
+    (s "1.12.0")
+    (tablist "1.0")
+    (transient "0.3.7"))
+  :commit "0899a56c41589e179d3b42994480802d8472e5b3" :authors
+  '(("Philippe Vaucher" . "philippe.vaucher@gmail.com"))
+  :maintainers
+  '(("Philippe Vaucher" . "philippe.vaucher@gmail.com"))
+  :maintainer
+  '("Philippe Vaucher" . "philippe.vaucher@gmail.com")
+  :keywords
+  '("filename" "convenience")
+  :url "https://github.com/Silex/docker.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
