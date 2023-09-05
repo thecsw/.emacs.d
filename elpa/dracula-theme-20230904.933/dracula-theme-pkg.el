2f19df730223fcc60a7e5bab4e3f93e8f480ea74
Sandy <sandy@sandyuraz.com>
2023-09-05 02:38:07 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/dracula-theme-20230904.933/dracula-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dracula-theme" "20230904.933" "Dracula Theme"
+  '((emacs "24.3"))
+  :commit "8e73c675816faadcf4d052b1ba09dc1b00aa1509" :authors
+  '(("film42"))
+  :maintainers
+  '(("Étienne Deparis" . "etienne@depar.is"))
+  :maintainer
+  '("Étienne Deparis" . "etienne@depar.is")
+  :url "https://github.com/dracula/emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/org-ref-20230830.9/org-ref-pkg.el
@@ -0,0 +1,24 @@
+(define-package "org-ref" "20230830.9" "citations, cross-references and bibliographies in org-mode"
+  '((org "9.4")
+    (dash "0")
+    (s "0")
+    (f "0")
+    (htmlize "0")
+    (hydra "0")
+    (avy "0")
+    (parsebib "0")
+    (bibtex-completion "0")
+    (citeproc "0")
+    (ox-pandoc "0"))
+  :commit "af103782f077bd4044062e723af52f9a4c591f5a" :authors
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainers
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainer
+  '("John Kitchin" . "jkitchin@andrew.cmu.edu")
+  :keywords
+  '("org-mode" "cite" "ref" "label")
+  :url "https://github.com/jkitchin/org-ref")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
