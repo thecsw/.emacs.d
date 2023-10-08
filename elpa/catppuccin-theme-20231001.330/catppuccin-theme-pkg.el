0ae5f8c3b79e25a7320b8d101ddb8cc37534e440
Sandy <sandy@sandyuraz.com>
2023-10-08 10:56:05 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/catppuccin-theme-20231001.330/catppuccin-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "catppuccin-theme" "20231001.330" "Catppuccin for Emacs - 🍄 Soothing pastel theme for Emacs"
+  '((emacs "25.1"))
+  :commit "9f93913e6d8cfbf7b224835c1ef85f7443d62d26" :authors
+  '(("nyxkrage"))
+  :maintainers
+  '(("Carsten Kragelund" . "carsten@kragelund.me"))
+  :maintainer
+  '("Carsten Kragelund" . "carsten@kragelund.me")
+  :url "https://github.com/catppuccin/emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/yabaki-theme-20231004.2023/yabaki-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "yabaki-theme" "20231004.2023" "Yabaki, the cast shadow"
+  '((emacs "27.1"))
+  :commit "209f2be321509dac00631fff1b0f7ea01ba382de" :authors
+  '(("David Goudou" . "david.goudou@gmail.com"))
+  :maintainers
+  '(("David Goudou" . "david.goudou@gmail.com"))
+  :maintainer
+  '("David Goudou" . "david.goudou@gmail.com")
+  :url "https://codeberg.org/seahorse/yabaki-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
