e9bd872829107ad49186bb7990c4241f97b418b6
Sandy <sandy@sandyuraz.com>
2023-10-11 01:42:10 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/company-20231009.1804/company-pkg.el
@@ -0,0 +1,14 @@
+(define-package "company" "20231009.1804" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "2b114a528ec3f1939b1d154d2cc48ddc7defe8a8" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/dashboard-20231010.2330/dashboard-pkg.el
@@ -0,0 +1,14 @@
+(define-package "dashboard" "20231010.2330" "A startup screen extracted from Spacemacs"
+  '((emacs "26.1"))
+  :commit "900375830951a6e492d2599d80e66075028e509b" :authors
+  '(("Rakan Al-Hneiti" . "rakan.alhneiti@gmail.com"))
+  :maintainers
+  '(("Jesús Martínez" . "jesusmartinez93@gmail.com"))
+  :maintainer
+  '("Jesús Martínez" . "jesusmartinez93@gmail.com")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
