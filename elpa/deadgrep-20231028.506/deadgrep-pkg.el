5a684887ee299ecc133dd2294be3e4f0fc771446
Sandy <sandy@sandyuraz.com>
2023-10-30 21:09:58 -0500

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/deadgrep-20231028.506/deadgrep-pkg.el
@@ -0,0 +1,17 @@
+(define-package "deadgrep" "20231028.506" "fast, friendly searching with ripgrep"
+  '((emacs "25.1")
+    (dash "2.12.0")
+    (s "1.11.0")
+    (spinner "1.7.3"))
+  :commit "960b61f4d9bcf75fa0f19c3abb447c63c7d886cf" :authors
+  '(("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainers
+  '(("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainer
+  '("Wilfred Hughes" . "me@wilfred.me.uk")
+  :keywords
+  '("tools")
+  :url "https://github.com/Wilfred/deadgrep")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
