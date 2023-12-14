1e002713dde04d730c4db900a70cd391a15e85e8
Sandy <sandy@sandyuraz.com>
2023-12-13 20:24:50 -0600

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/catppuccin-theme-20231213.1318/catppuccin-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "catppuccin-theme" "20231213.1318" "Catppuccin for Emacs - 🍄 Soothing pastel theme for Emacs"
+  '((emacs "25.1"))
+  :commit "1e9bdb0c19bf13c27a32b1adeabcace45fcbfd15" :authors
+  '(("nyxkrage"))
+  :maintainers
+  '(("Carsten Kragelund" . "carsten@kragelund.me"))
+  :maintainer
+  '("Carsten Kragelund" . "carsten@kragelund.me")
+  :url "https://github.com/catppuccin/emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
