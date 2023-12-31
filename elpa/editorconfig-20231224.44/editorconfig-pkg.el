5724a75af7bd095043fadc6be0c06578bf5e4512
Sandy <sandy@sandyuraz.com>
2023-12-30 19:34:31 -0600

    bump
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ elpa/editorconfig-20231224.44/editorconfig-pkg.el
@@ -0,0 +1,15 @@
+(define-package "editorconfig" "20231224.44" "EditorConfig Emacs Plugin"
+  '((emacs "26.1")
+    (nadvice "0.3"))
+  :commit "f7588dd1a216bfd0a89109ae7bcc3a7da74824c1" :authors
+  '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
+  :maintainers
+  '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
+  :maintainer
+  '("EditorConfig Team" . "editorconfig@googlegroups.com")
+  :keywords
+  '("convenience" "editorconfig")
+  :url "https://github.com/editorconfig/editorconfig-emacs#readme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-20231229.547/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20231229.547" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.7")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "4ab6bc9774e4f6a04f4b0d6fa37b9ae6c56100ce" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
