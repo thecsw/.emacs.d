c3d1be309d1a51c5323c78e76cdc70ac8d1b08e7
Sandy <sandy@sandyuraz.com>
2024-07-24 19:53:26 -0500

    bump

---
+++ elpa/centaur-tabs-20240716.2316/centaur-tabs-pkg.el
@@ -0,0 +1,15 @@
+(define-package "centaur-tabs" "20240716.2316" "Aesthetic, modern looking customizable tabs plugin"
+  '((emacs "27.1")
+    (powerline "2.4"))
+  :commit "4ff00b97d2bb316287695493754af4a7371749d5" :authors
+  '(("Emmanuel Bustos" . "ema2159@gmail.com"))
+  :maintainers
+  '(("Jen-Chieh Shen" . "jcs090218@gmail.com"))
+  :maintainer
+  '("Jen-Chieh Shen" . "jcs090218@gmail.com")
+  :keywords
+  '("frames")
+  :url "https://github.com/ema2159/centaur-tabs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/cuda-mode-20240716.1936/cuda-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cuda-mode" "20240716.1936" "NVIDIA CUDA Major Mode derived from C++-mode." 'nil :commit "7dd07a20c36b41c8cb38cdec989673b73d03e144" :authors
+  '(("Jack Morrison" . "jackmorrison1@gmail.com"))
+  :maintainers
+  '(("Jack Morrison" . "jackmorrison1@gmail.com"))
+  :maintainer
+  '("Jack Morrison" . "jackmorrison1@gmail.com")
+  :keywords
+  '("c" "languages" "cuda")
+  :url "https://github.com/chachi/cuda-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
