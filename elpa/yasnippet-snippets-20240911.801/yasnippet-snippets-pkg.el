7d0761924e6cd9ad7e0184fdf1921e48e80fdd28
Sandy <sandy@sandyuraz.com>
2024-09-11 22:10:18 -0500

    bump

---
+++ elpa/yasnippet-snippets-20240911.801/yasnippet-snippets-pkg.el
@@ -0,0 +1,14 @@
+(define-package "yasnippet-snippets" "20240911.801" "Collection of yasnippet snippets"
+  '((yasnippet "0.8.0"))
+  :commit "f9d12a931f803cd1a24bb20113bd716db8c01b48" :authors
+  '(("Andrea Crotti" . "andrea.crotti.0@gmail.com"))
+  :maintainers
+  '(("Andrea Crotti" . "andrea.crotti.0@gmail.com"))
+  :maintainer
+  '("Andrea Crotti" . "andrea.crotti.0@gmail.com")
+  :keywords
+  '("snippets")
+  :url "https://github.com/AndreaCrotti/yasnippet-snippets")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
